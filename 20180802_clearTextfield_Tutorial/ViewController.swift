1e2a8098d97b531a01e79e32bf6c74db54d3201b
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2018-08-02 19:53:08 +0800

    Initial Commit

---
+++ 20180802_clearTextfield_Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180802_clearTextfield_Tutorial
+//
+//  Created by chen hsin hung on 2018/8/2.
+//  Copyright © 2018年 chen hsin hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
